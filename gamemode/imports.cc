1e89827884ef3b61aa006e4484724470bd34b755
ff1f1ff10f0ff1ff0f1ff10 <brokernoreply@proton.me>
2026-03-28 22:55:54 +0100

    imports.cc

---
+++ gamemode/imports.cc
@@ -0,0 +1,121 @@
+Address	Ordinal	Name
+		
+msvcrt		
+000000018000A3C8		terminate(void)
+000000018000A3D0		__dllonexit
+000000018000A3D8		__CxxFrameHandler4
+000000018000A3E0		__C_specific_handler
+000000018000A3E8		_onexit
+000000018000A3F0		_lock
+000000018000A3F8		free
+000000018000A400		_amsg_exit
+000000018000A408		_CxxThrowException
+000000018000A410		_XcptFilter
+000000018000A418		_unlock
+000000018000A420		malloc
+000000018000A428		exception::exception(exception const &)
+000000018000A430		exception::exception(void)
+000000018000A438		exception::~exception(void)
+000000018000A440		operator delete(void *)
+000000018000A448		_initterm
+000000018000A450		memcpy_s
+000000018000A458		_vsnwprintf
+000000018000A460		type_info::~type_info(void)
+000000018000A468		memset
+api-ms-win-core-libraryloader-l1-2-0		
+000000018000A220		GetModuleHandleW
+000000018000A228		GetModuleHandleExW
+000000018000A230		DisableThreadLibraryCalls
+000000018000A238		GetModuleFileNameA
+000000018000A240		GetProcAddress
+api-ms-win-core-synch-l1-1-0		
+000000018000A2F0		WaitForSingleObject
+000000018000A2F8		ReleaseSemaphore
+000000018000A300		OpenSemaphoreW
+000000018000A308		CreateSemaphoreExW
+000000018000A310		CreateMutexExW
+000000018000A318		WaitForSingleObjectEx
+000000018000A320		ReleaseMutex
+api-ms-win-core-heap-l1-1-0		
+000000018000A200		GetProcessHeap
+000000018000A208		HeapFree
+000000018000A210		HeapAlloc
+api-ms-win-core-errorhandling-l1-1-0		
+000000018000A1C0		SetLastError
+000000018000A1C8		UnhandledExceptionFilter
+000000018000A1D0		RaiseException
+000000018000A1D8		GetLastError
+000000018000A1E0		SetUnhandledExceptionFilter
+api-ms-win-core-processthreads-l1-1-0		
+000000018000A260		OpenProcessToken
+000000018000A268		TerminateProcess
+000000018000A270		GetCurrentThreadId
+000000018000A278		GetCurrentProcess
+000000018000A280		GetCurrentProcessId
+api-ms-win-core-localization-l1-2-0		
+000000018000A250		FormatMessageW
+api-ms-win-core-debug-l1-1-0		
+000000018000A180		IsDebuggerPresent
+000000018000A188		DebugBreak
+000000018000A190		OutputDebugStringW
+api-ms-win-core-handle-l1-1-0		
+000000018000A1F0		CloseHandle
+api-ms-win-core-string-l1-1-0		
+000000018000A2E0		WideCharToMultiByte
+api-ms-win-eventing-provider-l1-1-0		
+000000018000A390		EventRegister
+000000018000A398		EventSetInformation
+000000018000A3A0		EventUnregister
+000000018000A3A8		EventWriteTransfer
+api-ms-win-core-winrt-string-l1-1-0		
+000000018000A370		WindowsDeleteString
+000000018000A378		WindowsGetStringRawBuffer
+000000018000A380		WindowsCreateStringReference
+api-ms-win-core-winrt-l1-1-0		
+000000018000A358		RoGetActivationFactory
+000000018000A360		RoActivateInstance
+api-ms-win-core-synch-l1-2-0		
+000000018000A330		Sleep
+api-ms-win-core-profile-l1-1-0		
+000000018000A2A0		QueryPerformanceCounter
+api-ms-win-core-sysinfo-l1-1-0		
+000000018000A340		GetSystemTimeAsFileTime
+000000018000A348		GetTickCount
+api-ms-win-core-rtlsupport-l1-1-0		
+000000018000A2C0		RtlCaptureContext
+000000018000A2C8		RtlLookupFunctionEntry
+000000018000A2D0		RtlVirtualUnwind
+ntdll		
+000000018000A478		RtlFreeHeap
+000000018000A480		RtlAllocateHeap
+000000018000A488		RtlNtStatusToDosErrorNoTeb
+000000018000A490		RtlCompareUnicodeString
+000000018000A498		RtlInitUnicodeString
+000000018000A4A0		RtlGetDeviceFamilyInfoEnum
+000000018000A4A8		NtQueryWnfStateData
+000000018000A4B0		NtQueryInformationToken
+api-ms-win-core-registry-l1-1-0		
+000000018000A2B0		RegGetValueW
+api-ms-win-core-processthreads-l1-1-1		
+000000018000A290		OpenProcess
+api-ms-win-appmodel-unlock-l1-1-0		
+000000018000A170		IsDeveloperModeEnabled
+api-ms-win-security-base-l1-1-0		
+000000018000A3B8		DuplicateTokenEx
+api-ms-win-core-delayload-l1-1-1		
+000000018000A1B0		ResolveDelayLoadedAPI
+api-ms-win-core-delayload-l1-1-0		
+000000018000A1A0		DelayLoadFailureHook
+ext-ms-win-resourcemanager-gamemode-l1-2-0		
+0000000180010040		RmGameModeRegisterProcess
+0000000180010048		RmGameModeInitializeResourceRequest
+0000000180010050		RmGameModeGetLargestValidResourceRequest
+0000000180010058		RmGameModeUnregisterProcess
+api-ms-win-core-com-l1-1-0		
+0000000180010000		CoSetProxyBlanket
+0000000180010008		CoGetCallContext
+ext-ms-win-core-resourcepolicy-l1-1-0		
+0000000180010028		FreeGameConfigStoreClient
+0000000180010030		CreateGameConfigStoreClient
+api-ms-win-security-capability-l1-1-0		
+0000000180010018		CapabilityCheck
